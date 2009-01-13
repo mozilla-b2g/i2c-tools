98bc667eeed8f0357a07a0536f6e00ba7f177f1e
khali <khali@7894878c-1315-0410-8ee3-d5d059ff63e0>
2009-01-13 17:07:57 +0000

    Licensing clarification.

--- include/Module.mk
+++ include/Module.mk
@@ -2,7 +2,10 @@
 #
 # Copyright (C) 2007  Jean Delvare <khali@linux-fr.org>
 #
-# Licensed under the GNU General Public License.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 INCLUDE_DIR	:= include
 
