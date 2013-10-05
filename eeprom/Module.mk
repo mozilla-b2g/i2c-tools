6ea50fa9d564cc17111560990d82529d3510379d
khali <khali@7894878c-1315-0410-8ee3-d5d059ff63e0>
2013-10-05 10:03:43 +0000

    Add manual pages for decode-dimms and decode-vaio
    Contributed by Jaromir Capik.

--- eeprom/Module.mk
+++ eeprom/Module.mk
@@ -10,19 +10,24 @@
 EEPROM_DIR	:= eeprom
 
 EEPROM_TARGETS	:= decode-dimms decode-vaio ddcmon decode-edid
+EEPROM_MANPAGES	:= decode-dimms.1 decode-vaio.1
 
 #
 # Commands
 #
 
 install-eeprom: $(addprefix $(EEPROM_DIR)/,$(EEPROM_TARGETS))
-	$(INSTALL_DIR) $(DESTDIR)$(bindir)
+	$(INSTALL_DIR) $(DESTDIR)$(bindir) $(DESTDIR)$(mandir)/man1
 	for program in $(EEPROM_TARGETS) ; do \
 	$(INSTALL_PROGRAM) $(EEPROM_DIR)/$$program $(DESTDIR)$(bindir) ; done
+	for manual in $(EEPROM_MANPAGES) ; do \
+	$(INSTALL_DATA) $(EEPROM_DIR)/$$manual $(DESTDIR)$(mandir)/man1 ; done
 
 uninstall-eeprom:
 	for program in $(EEPROM_TARGETS) ; do \
 	$(RM) $(DESTDIR)$(bindir)/$$program ; done
+	for manual in $(EEPROM_MANPAGES) ; do \
+	$(RM) $(DESTDIR)$(mandir)/$$manual ; done
 
 install: install-eeprom
 
